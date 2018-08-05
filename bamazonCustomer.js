0652e8a7ffdedcdeb0b1992e8ee503b88f2baeb8
unknown <monmaramba@yahoo.com>
2018-08-05 07:30:07 -0700

    renamed file per hw requirements

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,20 +4,19 @@ var inquirer = require("inquirer");
 var connection = mysql.createConnection({
   host: "localhost",
 
-  // Your port; if not 3306
   port: 3306,
 
   // Your username
   user: "root",
 
-  // Your password
   password: "DamnSQL1!",
   database: "bamazon_db
 });
 
-connection.connect(function(err, res) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    connection.end();
+  });
 
 
